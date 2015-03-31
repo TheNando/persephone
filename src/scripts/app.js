895cbaa012f1d45b29d04ec8955db74ce3c2ee3d
JJ4TH <jj4th@accasia.net>
2015-03-30 20:05:06 -0500

    Fix spacing
    You can consider this more or less a joke, I just wanted to submit a PR :D

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,15 +3,15 @@
 /* Create and configure application */
 
 angular
-	.module('persephone', [ 'ngMaterial' ])
-	.config([ '$mdThemingProvider', Routes])
-	.run([Run]);
+  .module('persephone', [ 'ngMaterial' ])
+  .config([ '$mdThemingProvider', Routes])
+  .run([Run]);
 
 function Routes ($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .accentPalette('brown')
-      .warnPalette('red');
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('brown')
+    .warnPalette('red');
 }
 
 function Run () {
